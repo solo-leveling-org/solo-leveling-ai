a018b41ecfdf3d29ec97647a656d2f7f41f0116d
sleepkqq <nikita32ivanov@gmail.com>
2025-03-05 09:01:55 +0300

    initial commit

---
+++ src/test/java/com/sleepkqq/sololeveling/ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleepkqq.sololeveling.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
