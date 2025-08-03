cf058e324a8e3df527f6f6b7261d7cc8fbce2aa9
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-03 12:24:23 +0300

    Kafka, Schema Registry and Redis configuration (#2)
    * Refactored deployment to use service names.
    
    * 60s -> 90s

--- solo-leveling-ai-service/src/test/kotlin/com/sleepkqq/sololeveling/ai/BaseTestClass.kt
+++ solo-leveling-ai-service/src/test/kotlin/com/sleepkqq/sololeveling/ai/BaseTestClass.kt
@@ -2,17 +2,14 @@ package com.sleepkqq.sololeveling.ai
 
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.context.bean.override.mockito.MockitoBean
-import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.DockerImageName
+import org.testcontainers.kafka.ConfluentKafkaContainer
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class BaseTestClass {
+abstract class BaseTestClass {
 
 	@MockitoBean
 	lateinit var chatClient: ChatClient
@@ -20,12 +17,6 @@ class BaseTestClass {
 	companion object {
 		@JvmStatic
 		@Container
-		val kafkaContainer = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"))
-
-		@DynamicPropertySource
-		@JvmStatic
-		fun setKafkaProperties(registry: DynamicPropertyRegistry) {
-			registry.add("spring.kafka.bootstrap-servers") { kafkaContainer.bootstrapServers }
-		}
+		val kafkaContainer = ConfluentKafkaContainer("confluentinc/cp-kafka:latest")
 	}
 }
