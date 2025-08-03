d15e32a72ab55ee23b5df89e1cb7ef292441cd88
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-03 15:48:04 +0300

    New field task 'currencyReward' resolving logic (#4)
    * task.currencyReward resolving logic and refactored DTO mapping using MapStruct.
    
    * Reduces chat model temperature to 1.2.

--- solo-leveling-ai-service/src/main/kotlin/com/sleepkqq/sololeveling/ai/config/AiConfig.kt
+++ solo-leveling-ai-service/src/main/kotlin/com/sleepkqq/sololeveling/ai/config/AiConfig.kt
@@ -1,19 +1,22 @@
 package com.sleepkqq.sololeveling.ai.config
 
 import com.sleepkqq.sololeveling.ai.prompt.TaskPrompts
-import org.modelmapper.ModelMapper
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.prompt.ChatOptions
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+@Suppress("unused")
 @Configuration
 class AiConfig {
 
 	@Bean
-	fun modelMapper(): ModelMapper = ModelMapper()
-
-	@Bean
 	fun chatClient(builder: ChatClient.Builder): ChatClient = builder
+		.defaultOptions(
+			ChatOptions.builder()
+				.temperature(1.2)
+				.build()
+		)
 		.defaultSystem(TaskPrompts.GENERATE_TASK_SYSTEM_PROMPT)
 		.build()
 }
--- solo-leveling-ai-service/src/main/kotlin/com/sleepkqq/sololeveling/ai/kafka/GenerateTasksConsumer.kt
+++ solo-leveling-ai-service/src/main/kotlin/com/sleepkqq/sololeveling/ai/kafka/GenerateTasksConsumer.kt
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
+@Suppress("unused")
 @Service
 class GenerateTasksConsumer(
 	private val chatService: ChatService,
