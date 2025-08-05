b6c8ccdda3169e7c1a08bfd7bd96713af7476f08
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-05 21:50:08 +0300

    Schema registry currencyReward field fix (#6)
    * Updates solo-leveling-avro dependency to version 2.1.3.
    
    * Refactored CI/CD workflows for Dockerized deployment.
    
    * Removed BigDecimal conversion; uses Int for currency.

--- solo-leveling-ai-service/src/main/kotlin/com/sleepkqq/sololeveling/ai/mapper/AvroMapper.kt
+++ solo-leveling-ai-service/src/main/kotlin/com/sleepkqq/sololeveling/ai/mapper/AvroMapper.kt
@@ -1,18 +1,12 @@
 package com.sleepkqq.sololeveling.ai.mapper
 
-import com.sleepkqq.sololeveling.ai.extensions.toByteBuffer
 import com.sleepkqq.sololeveling.ai.model.GenerateTaskResponse
 import com.sleepkqq.sololeveling.avro.task.GenerateTask
 import com.sleepkqq.sololeveling.avro.task.SaveTask
 import org.mapstruct.CollectionMappingStrategy
 import org.mapstruct.Mapper
-import org.mapstruct.Mapping
-import org.mapstruct.Named
 import org.mapstruct.ReportingPolicy
 import org.springframework.stereotype.Component
-import java.math.BigDecimal
-import java.nio.ByteBuffer
-
 
 @Component
 @Mapper(
@@ -22,13 +16,5 @@ import java.nio.ByteBuffer
 )
 abstract class AvroMapper {
 
-	@Named("toByteBuffer")
-	fun map(input: BigDecimal): ByteBuffer = input.toByteBuffer()
-
-	@Mapping(
-		target = "currencyReward",
-		source = "response.currencyReward",
-		qualifiedByName = ["toByteBuffer"]
-	)
 	abstract fun map(response: GenerateTaskResponse, input: GenerateTask): SaveTask
 }
